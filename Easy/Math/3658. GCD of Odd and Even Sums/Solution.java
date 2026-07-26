7a2d22f8a082523b1a48568da71d712313e6c3aa
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-26 10:03:09 +0530

    Create Solution.java

---
+++ Easy/Math/3658. GCD of Odd and Even Sums/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+
+        //Sum of first n odd numbers is n² and sum of first n even numbers is n(n+1). Their GCD is n.
+      
+        return n;
+    }
+}
