2b1fa5ef362df873110b24406399146d6fba2d73
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-07-26 11:01:46 +0530

    Create Solution.java

---
+++ Easy/Math/3783. Mirror Distance of an Integer/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mirrorDistance(int n) {
+
+        int rev = 0;
+        int temp = n;
+
+        while(temp > 0){
+            
+            int digit = temp % 10;
+            rev = rev * 10 + digit;
+            temp /= 10;
+        }
+
+        return Math.abs(n - rev);
+    }
+}
