bf47c69251eb529371dc729ba6cd76a3cba7f1ba
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-05 07:25:24 +0530

    Create Solution.java

---
+++ Easy/Array/3190. Find Minimum Operations to Make All Elements Divisible by Three/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+
+        for(int n: nums){
+            int r = n % 3;
+            count += Math.min(r, 3 - r);
+        }
+        
+        return count;
+    }
+}
