dddfba9d0047c6fc2c75652ae61d19458042b2c2
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-06 07:53:43 +0530

    Create Solution.java

---
+++ Easy/Array/3838. Weighted Word Mapping/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+      
+        StringBuilder sb = new StringBuilder();
+
+        for(String word : words){
+          
+            int sum = 0;
+
+            for(int i = 0; i < word.length(); i++){
+                sum += weights[word.charAt(i) - 'a'];
+            }
+
+            int mod = sum % 26;
+            
+            sb.append((char) ('z' - mod));
+        }
+
+        return sb.toString();
+    }
+}
