aa60da9e76e55878d6f2aa78d66cdfd9832707fb
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-09 10:47:27 +0530

    Create Solution.kt

---
+++ Easy/Array/2942. Find Words Containing Character/Solution.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun findWordsContaining(words: Array<String>, x: Char): List<Int> {
+
+        val result = mutableListOf<Int>()
+
+        for(i in words.indices) {
+            if(words[i].contains(x)) {
+                result.add(i)
+            }
+        }
+
+        return result
+    }
+}
