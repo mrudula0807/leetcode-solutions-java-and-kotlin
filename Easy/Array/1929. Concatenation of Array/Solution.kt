87f6a2e722692770cb69bb1ad5701674b4bf4054
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-11 11:26:12 +0530

    Create Solution.kt

---
+++ Easy/Array/1929. Concatenation of Array/Solution.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+
+        val n = nums.size
+        val result = IntArray(2 * n)
+
+        for(i in nums.indices){
+            result[i] = nums[i]
+            result[i + n] = nums[i]
+        }
+
+        return result
+    }
+}
