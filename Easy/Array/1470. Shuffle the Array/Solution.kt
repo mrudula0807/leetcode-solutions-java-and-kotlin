3eefb9d2b07ae59cdb98a804e673fbd19f13cc26
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-18 17:18:26 +0530

    Create Solution.kt

---
+++ Easy/Array/1470. Shuffle the Array/Solution.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun shuffle(nums: IntArray, n: Int): IntArray {
+        
+        val result = IntArray(2 * n)
+
+        for(i in 0 until n){
+            result[2 * i] = nums[i]
+            result[2 * i + 1] = nums[i + n]
+        }
+
+        return result
+    }
+}
