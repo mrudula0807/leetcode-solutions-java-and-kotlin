230024643927f87e6741e5469146d259311c70d6
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-08-25 09:54:05 +0530

    Implement running sum function in Solution.kt

---
+++ Easy/Array/1480. Running Sum of 1d Array/Solution.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun runningSum(nums: IntArray): IntArray {
+        
+        for(i in 1 until nums.size) {
+            nums[i] += nums[i - 1]
+        }
+
+        return nums
+    }
+}
