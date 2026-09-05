a6d44efff5fc721561b1d832f3241fafa2af8d2f
Mrudula Sreekumar <50162773+mrudula0807@users.noreply.github.com>
2026-09-05 10:15:57 +0530

    Create Solution.java

---
+++ Easy/Array/2974. Minimum Number Game/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int[] arr = new int[nums.length];
+
+        for(int i = 0; i < nums.length - 1; i += 2){
+            arr[i] = nums[i + 1];
+            arr[i + 1] = nums[i];
+        }
+
+        return arr;
+    }
+}
